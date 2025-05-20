90a429887d67226e11aa87fe32427233aca35716
huadyng <huydang882125@gmail.com>
2025-05-20 22:01:05 +0700

    update hình nền
    asdfasdf

--- Frontend/ferticare-front/src/App.js
+++ Frontend/ferticare-front/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hẹ hẹ
+          hẹ hẹ hello
         </p>
         <a
           className="App-link"
