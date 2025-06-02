0bf90ee69aa61ccbf5d51d49829d16d45a73973e
phucplhse183189 <phucplhse183189@fpt.edu.vn>
2025-06-02 18:09:39 +0700

    Update trang HomePage (1)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Footer from "./components/layout/Footer/Footer";
 import Header from "./components/layout/Header/Header";
 import HeroSection from "./components/Pages/HomePage/HeroSection";
 import Login from "./components/Pages/Login/Login";
+import AboutUs from "./components/Pages/HomePage/AboutUs";
+import Service from "./components/Pages/HomePage/Service";
+import DoctorCarousel from "./components/Pages/HomePage/DoctorCarousel";
+import AchievementPage from "./components/Pages/HomePage/AchievementPage";
+import NewsPage from "./components/Pages/HomePage/NewsPage";
+import FeedbackPage from "./components/Pages/HomePage/FeedbackPage";
 
 function AppContent() {
   const location = useLocation();
@@ -16,6 +22,12 @@ function AppContent() {
         <Route path="/" element={<HeroSection />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      {!isLoginPage && <AboutUs />}
+      {!isLoginPage && <Service />}
+      {!isLoginPage && <DoctorCarousel />}
+      {!isLoginPage && <AchievementPage />}
+      {!isLoginPage && <NewsPage />}
+      {!isLoginPage && <FeedbackPage />}
 
       {!isLoginPage && <Footer />}
     </>
